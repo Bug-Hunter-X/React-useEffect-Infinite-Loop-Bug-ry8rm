35f6bb8d570e9ea787ec7b95c04fa7236f955c5d
BugHunter-X <askzenai@gmail.com>
2025-02-19 00:48:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect logic: This will cause an infinite loop
+    setCount(count + 1);
+  }, [count]); // count is in the dependency array
+
+  return <div>Count: {count}</div>;
+}
+```
